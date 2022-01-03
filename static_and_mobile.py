2484cf40dacd82e795ed182b9d28c0fd84c76f38
lws <wawnx716@gmail.com>
2022-01-03 11:16:22 -0800

    delete unused script

--- static_and_mobile.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-from parse_shark import get_range_and_ul_count
-from parse_tottag import get_rssi, get_raw
-from parse_bot import get_log, get_yaws
-import os
-from bisect import bisect_left
-import math
-import matplotlib.pyplot as plt
